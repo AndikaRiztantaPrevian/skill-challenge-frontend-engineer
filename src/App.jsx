135eab04acb5c59b4e99d97b62862970f05437e9
piaan <VianRiztanta71@gmail.com>
2024-08-16 15:42:06 +0700

    fix bubble menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import "./App.css";
 import { useState } from "react";
 import Sidebar from "./components/sidebar/sidebarComponent";
 import Header from "./components/header/headerCompoonent";
@@ -11,8 +10,14 @@ function App() {
     setIsBubbleClicked(!isBubbleClicked);
   };
 
-  const handleButtonClick = () => {
-    setSelectedButton(buttonType);
+  const handleButtonClick = (buttonType) => {
+    if (selectedButton === buttonType) {
+      setIsBubbleClicked(true);
+      setSelectedButton(null);
+    } else {
+      setIsBubbleClicked(false);
+      setSelectedButton(buttonType);
+    }
   };
 
   return (
@@ -28,53 +33,65 @@ function App() {
           {/* Content */}
           <div className="bg-primary-gray-darken h-[92.75%] flex justify-end items-end p-5">
             <div className="w-fit h-[90px] flex flex-row-reverse justify-end items-center relative py-2">
+              {/* Button Bubble */}
               {!selectedButton && (
-                <>
-                  {/* Button Bubble */}
-                  <button
-                    type="button"
-                    id="buttonBubble"
-                    onClick={handleBubbleClick}
-                    className={`w-12 h-12 bg-primary-blue rounded-full self-end shadow-md z-50 relative`}
-                  ></button>
-                  {/* Button Inbox */}
-                  <div
-                    className={`flex flex-col items-center absolute z-1 transition-all duration-500 transform ${
-                      isBubbleClicked
-                        ? "translate-x-[-60px] opacity-100"
-                        : "translate-x-[0px] opacity-0"
-                    }`}
-                  >
-                    <span className="font-latoBold text-sm text-primary-white mb-1">
-                      Inbox
-                    </span>
-                    <button
-                      type="button"
-                      id="buttonModalInbox"
-                      onClick={() => handleButtonClick("Inbox")}
-                      className="w-10 h-10 bg-primary-white rounded-full shadow-md"
-                    ></button>
-                  </div>
-                  {/* Button Inbox */}
-                  <div
-                    className={`flex flex-col items-center absolute z-20 transition-all duration-500 transform ${
-                      isBubbleClicked
-                        ? "translate-x-[-115px] opacity-100"
-                        : "translate-x-[0px] opacity-0"
-                    }`}
-                  >
-                    <span className="font-latoBold text-sm text-primary-white mb-1">
-                      Task
-                    </span>
-                    <button
-                      type="button"
-                      id="buttonModalTask"
-                      onClick={() => handleButtonClick("Task")}
-                      className="w-10 h-10 bg-primary-white rounded-full shadow-md"
-                    ></button>
-                  </div>
-                </>
+                <button
+                  type="button"
+                  id="buttonBubble"
+                  onClick={handleBubbleClick}
+                  className={`w-12 h-12 bg-primary-blue rounded-full self-end shadow-md relative z-30`}
+                ></button>
               )}
+
+              {/* Button Inbox */}
+              <div
+                className={`flex flex-col h-full ${selectedButton === "Inbox" ? "justify-end" : "justify-center"} py-2 items-center absolute transition-all duration-500 z-20 transform ${
+                  isBubbleClicked
+                    ? "translate-x-[-5px]"
+                    : "translate-x-[0px]"
+                } ${!isBubbleClicked && !selectedButton ? "hidden" : ""}`}
+                style={{
+                  right: selectedButton === "Inbox" ? "-0px" : "70px",
+                  zIndex: selectedButton === "Inbox" ? 50 : 20,
+                }}
+              >
+                <span className={`font-latoBold text-[0.7rem] text-primary-white mb-1 ${selectedButton ? "hidden" : ""}`}>
+                  Inbox
+                </span>
+                <button
+                  type="button"
+                  id="buttonModalInbox"
+                  onClick={() => handleButtonClick("Inbox")}
+                  className={`${
+                    selectedButton === "Inbox" ? "w-12 h-12 bg-indicator-orange" : "w-10 h-10 bg-primary-white mt-6"
+                  } ${isBubbleClicked ? "mt-0" : ""} rounded-full shadow-md`}
+                ></button>
+              </div>
+
+              {/* Button Task */}
+              <div
+                className={`flex flex-col h-full ${selectedButton === "Task" ? "justify-end" : "justify-center"} py-2 items-center absolute transition-all duration-500 z-10 transform ${
+                  isBubbleClicked
+                    ? "translate-x-[-70px]"
+                    : "translate-x-[0px]"
+                } ${!isBubbleClicked && !selectedButton ? "hidden" : ""}`}
+                style={{
+                  right: selectedButton === "Task" ? "-0px" : "70px",
+                  zIndex: selectedButton === "Task" ? 50 : 10,
+                }}
+              >
+                <span className={`font-latoBold text-[0.7rem] text-primary-white mb-1 ${selectedButton ? "hidden" : ""}`}>
+                  Task
+                </span>
+                <button
+                  type="button"
+                  id="buttonModalTask"
+                  onClick={() => handleButtonClick("Task")}
+                  className={`${
+                    selectedButton === "Task" ? "w-12 h-12 bg-indicator-purple" : "w-10 h-10 bg-primary-white mt-6"
+                  } ${isBubbleClicked ? "mt-0" : ""} rounded-full shadow-md`}
+                ></button>
+              </div>
             </div>
           </div>
         </div>
